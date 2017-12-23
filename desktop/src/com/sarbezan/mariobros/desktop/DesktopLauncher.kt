f3572bb08683e269aab1493d138b3427f9f2cc90
hossein <hossein.zolfi@gmail.com>
2017-12-23 22:20:09 +0330

    Run the app

---
+++ desktop/src/com/sarbezan/mariobros/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package com.sarbezan.mariobros.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.sarbezan.mariobros.MarioBros
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(MarioBros(), config)
+    }
+}
