1f44edd1efa9f3ff6c9ef76e453af42ef769818d
hossein <hossein.zolfi@gmail.com>
2017-12-23 23:02:59 +0330

    Learning how to use ViewPort and Camera. setProjectMatrix to camera.combined and resize viewPort.

--- core/src/com/sarbezan/mariobros/screens/PlayerScreen.kt
+++ core/src/com/sarbezan/mariobros/screens/PlayerScreen.kt
@@ -3,11 +3,16 @@ package com.sarbezan.mariobros.screens
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.utils.viewport.FitViewport
+import com.badlogic.gdx.utils.viewport.Viewport
 import com.sarbezan.mariobros.MarioBros
 
 class PlayerScreen(private val game: MarioBros) : Screen {
-    private var img: Texture = Texture("badlogic.jpg")
+    private val img: Texture = Texture("badlogic.jpg")
+    private val gameCam = OrthographicCamera()
+    private val gamePort: Viewport = FitViewport(800f, 480f, gameCam)
 
     override fun hide() {
     }
@@ -18,9 +23,12 @@ class PlayerScreen(private val game: MarioBros) : Screen {
     override fun render(delta: Float) {
         Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        game.batch.begin()
-        game.batch.draw(img, 0f, 0f)
-        game.batch.end()
+        with(game.batch) {
+            projectionMatrix = gameCam.combined
+            begin()
+            draw(img, 0f, 0f)
+            end()
+        }
 
     }
 
@@ -31,6 +39,7 @@ class PlayerScreen(private val game: MarioBros) : Screen {
     }
 
     override fun resize(width: Int, height: Int) {
+        gamePort.update(width, height)
     }
 
     override fun dispose() {
