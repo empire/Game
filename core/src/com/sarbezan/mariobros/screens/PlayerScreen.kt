c5952db374dd0d47af1f104c0f96627f7f47b4e4
hossein <hossein.zolfi@gmail.com>
2017-12-26 21:41:14 +0330

    Using LibGdx Box2d for physic engine. First I create World instance, each world contains bodies, so I call world.createBody to get new Body instance, this method get BodyDef as parameter. For each body must create fixture by calling body.createFixture. with FixtureDef as parameter.
    BodyDef has position and BodyType (StaticBody, DynamicBody, KineticBody)
    FixtureDef has shape (PolygonShape with width and height)

--- core/src/com/sarbezan/mariobros/screens/PlayerScreen.kt
+++ core/src/com/sarbezan/mariobros/screens/PlayerScreen.kt
@@ -4,8 +4,11 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.maps.objects.RectangleMapObject
 import com.badlogic.gdx.maps.tiled.TmxMapLoader
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.physics.box2d.*
 import com.badlogic.gdx.utils.viewport.FitViewport
 import com.badlogic.gdx.utils.viewport.Viewport
 import com.sarbezan.mariobros.MarioBros
@@ -20,8 +23,13 @@ class PlayerScreen(private val game: MarioBros) : Screen {
     private val map = mapLoader.load("level1.tmx")
     private val renderer = OrthogonalTiledMapRenderer(map, game.batch)
 
+    private val world = World(Vector2(0f, 0f), true)
+    private val b2dRenderer = Box2DDebugRenderer()
+
     init {
         gameCam.position.set(gamePort.worldWidth / 2f, gamePort.worldHeight / 2f, 0f)
+
+        wrapShapes()
     }
 
     override fun hide() {
@@ -40,6 +48,7 @@ class PlayerScreen(private val game: MarioBros) : Screen {
             projectionMatrix = hud.combined
             hud.draw()
         }
+        b2dRenderer.render(world, gameCam.combined)
 
     }
 
@@ -52,6 +61,65 @@ class PlayerScreen(private val game: MarioBros) : Screen {
     private fun handleInput(delta: Float) {
         if (Gdx.input.isTouched) {
             gameCam.position.x += 100 * delta
+        }
+    }
+
+    private fun wrapShapes() {
+        val bodyDef = BodyDef()
+        val shape = PolygonShape()
+        val fixtureDef = FixtureDef()
+
+        // Ground
+        for (mapObject in map.layers[2].objects.getByType(RectangleMapObject::class.java)) {
+            with(mapObject.rectangle) {
+                bodyDef.position.set(x + width / 2, y + height / 2)
+                bodyDef.type = BodyDef.BodyType.StaticBody
+                val body = world.createBody(bodyDef);
+
+
+                shape.setAsBox(width / 2, height / 2)
+                fixtureDef.shape = shape
+                body.createFixture(fixtureDef)
+            }
+        }
+
+        // Pipes
+        for (mapObject in map.layers[3].objects.getByType(RectangleMapObject::class.java)) {
+            bodyDef.type = BodyDef.BodyType.StaticBody
+            fixtureDef.shape = shape
+            with(mapObject.rectangle) {
+                bodyDef.position.set(x + width / 2, y + height / 2)
+                shape.setAsBox(width / 2, height / 2)
+            }
+            val body = world.createBody(bodyDef);
+
+            body.createFixture(fixtureDef)
+        }
+
+        // Coins
+        for (mapObject in map.layers[4].objects.getByType(RectangleMapObject::class.java)) {
+            bodyDef.type = BodyDef.BodyType.StaticBody
+            fixtureDef.shape = shape
+            with(mapObject.rectangle) {
+                bodyDef.position.set(x + width / 2, y + height / 2)
+                shape.setAsBox(width / 2, height / 2)
+            }
+            val body = world.createBody(bodyDef);
+
+            body.createFixture(fixtureDef)
+        }
+
+        // Bricks
+        for (mapObject in map.layers[5].objects.getByType(RectangleMapObject::class.java)) {
+            bodyDef.type = BodyDef.BodyType.StaticBody
+            fixtureDef.shape = shape
+            with(mapObject.rectangle) {
+                bodyDef.position.set(x + width / 2, y + height / 2)
+                shape.setAsBox(width / 2, height / 2)
+            }
+            val body = world.createBody(bodyDef);
+
+            body.createFixture(fixtureDef)
         }
     }
 
